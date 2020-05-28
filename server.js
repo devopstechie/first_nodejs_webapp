3fb6d45970a49b1512e0333e4ace24890102a783
devopstechie <61161851+devopstechie@users.noreply.github.com>
2020-05-28 22:27:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-'use strict';
-
+const http = require('http');
 const express = require('express');
-
-// Constants
-const PORT = 3000;
-const HOST = '0.0.0.0';
-
-// App
+const path = require('path');
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello Azure Devops!\n');
-});
-
-var port = process.env.PORT||PORT;
-app.listen(port);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.use(express.json());
+app.use(express.static("express"));
+// default URL for website
+app.use('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/express/index.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+console.debug('Server listening on port ' + port);
